4481dbb0179c7db2c45647285c769ae71388f986
Sebastien Riou <sebastien.riou@pqshield.com>
2026-04-19 17:24:30 +0200

    improve test targets

--- libcmtrace/source/test_targets.c
+++ libcmtrace/source/test_targets.c
@@ -1,24 +1,24 @@
 #include <stdint.h>
-uint64_t umul64(uint64_t a, uint64_t b){
+__attribute__((optimize("align-functions=4"))) uint64_t umul64(uint64_t a, uint64_t b){
 	return a*b;
 }
 
 volatile uint64_t vbuf64[2] = {0};
 volatile uint64_t sink64;
-void test_umul64_core(){
+__attribute__((optimize("align-functions=4"))) void test_umul64_core(){
 	sink64 = umul64(vbuf64[0],vbuf64[1]);
 }
-void test_umul64_16_16(){
+__attribute__((optimize("align-functions=4"))) void test_umul64_16_16(){
 	vbuf64[0] = 0xFFFF;
 	vbuf64[1] = 0xFFFF;
 	test_umul64_core();
 }
-void test_umul64_32_16(){
+__attribute__((optimize("align-functions=4"))) void test_umul64_32_16(){
 	vbuf64[0] = 0xFFFFFFFF;
 	vbuf64[1] = 0xFFFF;
 	test_umul64_core();
 }
-void test_umul64_32_32(){
+__attribute__((optimize("align-functions=4"))) void test_umul64_32_32(){
 	vbuf64[0] = 0xFFFFFFFF;
 	vbuf64[1] = 0xFFFFFFFF;
 	test_umul64_core();
