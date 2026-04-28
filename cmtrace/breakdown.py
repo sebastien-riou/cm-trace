68ba5ae0316a4cf1f29425d184a9f1c8429c9d7c
Sebastien Riou <sebastien.riou@pqshield.com>
2026-04-28 22:18:52 +0200

    breakdown WIP

---
+++ cmtrace/breakdown.py
@@ -0,0 +1,29 @@
+import argparse
+
+import logging
+from cmtrace import CmTrace, CustomScale
+import os
+
+
+def main():
+    scriptname = os.path.basename(__file__)
+    parser = argparse.ArgumentParser(scriptname)
+    levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
+    parser.add_argument('--log-level', default='INFO', choices=levels)
+    parser.add_argument('trace_path', metavar='trace-path', help='Path to the trace file', type=str)
+    
+    
+    args = parser.parse_args()
+
+    logformat = '%(asctime)s.%(msecs)03d %(levelname)s:\t%(message)s'
+    logdatefmt = '%Y-%m-%d %H:%M:%S'
+    logging.basicConfig(level=args.log_level, format=logformat, datefmt=logdatefmt)
+
+    trace = CmTrace.from_file(args.trace_path)
+    logging.debug(f'{trace.instruction_count} instructions, {trace.total_cycles} cycles')
+    
+    trace.breakdown()
+
+
+if __name__ == '__main__':
+    main()
